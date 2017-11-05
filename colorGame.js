e990a208be947c1161f06a6ccfa2193697de51d0
DRAJ <davidrajnus@gmail.com>
2017-11-06 02:03:33 +0800

    added webkit transition and beautify css html

--- colorGame.js
+++ colorGame.js
@@ -54,7 +54,7 @@ resetButton.addEventListener("click", function(){
     //add initial colors to squares
     squares[i].style.background = colors[i];
   };
-  h1.style.background = "#232323";
+  h1.style.background = "steelblue";
 })
 
 colorDisplay.textContent = pickedColor;
