e7ca34ca863339f13b6d29f86b840046656b785c
DRAJ <davidrajnus@gmail.com>
2017-11-06 00:41:28 +0800

    included messages for correct answers

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,10 @@
-var colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0 , 255)",
-  "rgb(255, 0, 255)",
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,9 +18,47 @@ for (var i = 0; i < squares.length; i++){
     var clickedColor = this.style.background;
     //compare color to pickedColor
     if (clickedColor === pickedColor) {
-      alert("Correct!");
+      messageDisplay.textContent = "Correct!";
+      h1.style.background = clickedColor;
+      //change everything to correct color
+      changeColors(pickedColor);
     } else {
-      alert("Wrong!");
+      this.style.background = "black";
+      messageDisplay.textContent = "Try again";
     };
   });
 };
+
+function changeColors(color) {
+  for (var i = 0; i < squares.length; i++){
+    squares[i].style.background = color;
+  };
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random()*colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+  //make an array
+  var arr = [];
+  //repeat num times
+  for(var i = 0; i < num; i++) {
+    //get random colors and push into arr
+    arr.push(randomColor());
+  };
+  return arr
+}
+
+function randomColor() {
+  //pick R between 0 to 255
+  var r = Math.floor(Math.random() * 256);
+  //pick G between 0 to 255
+  var g = Math.floor(Math.random() * 256);
+  //pick B between 0 to 255
+  var b = Math.floor(Math.random() * 256);
+
+  return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
